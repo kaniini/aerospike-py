9dbb8b7410e382b852c8c43b4fe86bba347992dd
William Pitcock <nenolod@dereferenced.org>
2016-01-26 17:19:47 -0500

    query-info: cleanups

--- query-info.py
+++ query-info.py
@@ -5,8 +5,8 @@ from aerospike_py.connection import SocketConnection
 
 sck = SocketConnection(socket.create_connection((sys.argv[1], 3000)))
 header, infokeys = request_info_keys(sck, [
-    'build', 'edition', 'node', 'replicas-read', 'replicas-write', 'service', 'services', 'statistics', 'version'
+    'build', 'edition', 'node', 'service', 'services', 'statistics', 'version'
 ])
 
 for k, v in infokeys.items():
-    print("%-15s: %s\n" % (k, v))
+    print("%-15s: %s" % (k, v))
