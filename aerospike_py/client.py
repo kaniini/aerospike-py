fcd15765f60d63232f75d70271bdd4c95d1fa6ad
William Pitcock <nenolod@dereferenced.org>
2016-01-31 22:41:35 -0500

    client: allow record_ttl to be set

--- aerospike_py/client.py
+++ aerospike_py/client.py
@@ -20,13 +20,13 @@ class AerospikeClient:
     def info(self, keys):
         return request_info_keys(self.sck, keys)[1]
 
-    def get(self, namespace, set='', key='', bins=[]):
+    def get(self, namespace, set='', key='', bins=[], record_ttl=0):
         flags = aerospike_py.message.AS_INFO1_READ
         if not bins:
             flags |= aerospike_py.message.AS_INFO1_GET_ALL
 
         bin_cmds = [aerospike_py.message.pack_asmsg_operation(aerospike_py.message.AS_MSG_OP_READ, 0, bn, b'') for bn in bins]
-        envelope = aerospike_py.message.pack_asmsg(flags, 0, 0, 0, 0, 0,
+        envelope = aerospike_py.message.pack_asmsg(flags, 0, 0, 0, record_ttl, 0,
             [
                 aerospike_py.message.pack_asmsg_field(namespace.encode('UTF-8'), aerospike_py.message.AS_MSG_FIELD_TYPE_NAMESPACE),
                 aerospike_py.message.pack_asmsg_field(hash_key(set, key), aerospike_py.message.AS_MSG_FIELD_TYPE_DIGEST_RIPE),
@@ -42,7 +42,7 @@ class AerospikeClient:
 
         return buckets
 
-    def put(self, namespace, set='', key='', bins={}, create_only=False, bin_create_only=False):
+    def put(self, namespace, set='', key='', bins={}, create_only=False, bin_create_only=False, record_ttl=0):
         flags = aerospike_py.message.AS_INFO2_WRITE
         if create_only:
             flags |= aerospike_py.message.AS_INFO2_CREATE_ONLY
@@ -52,7 +52,7 @@ class AerospikeClient:
 
         encoded_bins = [(k, aerospike_py.message.encode_payload(v)) for k, v in bins.items()]
         bin_cmds = [aerospike_py.message.pack_asmsg_operation(aerospike_py.message.AS_MSG_OP_WRITE, i[1][1], i[0], i[1][0]) for i in encoded_bins]
-        envelope = aerospike_py.message.pack_asmsg(0, flags, 0, 0, 0, 0,
+        envelope = aerospike_py.message.pack_asmsg(0, flags, 0, 0, record_ttl, 0,
             [
                 aerospike_py.message.pack_asmsg_field(namespace.encode('UTF-8'), aerospike_py.message.AS_MSG_FIELD_TYPE_NAMESPACE),
                 aerospike_py.message.pack_asmsg_field(hash_key(set, key), aerospike_py.message.AS_MSG_FIELD_TYPE_DIGEST_RIPE),
@@ -68,8 +68,8 @@ class AerospikeClient:
 
         return buckets
 
-    def delete(self, namespace, set='', key=''):
-        envelope = aerospike_py.message.pack_asmsg(0, aerospike_py.message.AS_INFO2_WRITE | aerospike_py.message.AS_INFO2_DELETE, 0, 0, 0, 0,
+    def delete(self, namespace, set='', key='', record_ttl=0):
+        envelope = aerospike_py.message.pack_asmsg(0, aerospike_py.message.AS_INFO2_WRITE | aerospike_py.message.AS_INFO2_DELETE, 0, 0, record_ttl, 0,
             [
                 aerospike_py.message.pack_asmsg_field(namespace.encode('UTF-8'), aerospike_py.message.AS_MSG_FIELD_TYPE_NAMESPACE),
                 aerospike_py.message.pack_asmsg_field(hash_key(set, key), aerospike_py.message.AS_MSG_FIELD_TYPE_DIGEST_RIPE),
@@ -86,11 +86,11 @@ class AerospikeClient:
         return buckets
 
 
-    def incr(self, namespace, set='', key='', bin='', incr_by=0):
+    def incr(self, namespace, set='', key='', bin='', incr_by=0, record_ttl=0):
         flags = aerospike_py.message.AS_INFO2_WRITE
 
         bin_cmds = [aerospike_py.message.pack_asmsg_operation(aerospike_py.message.AS_MSG_OP_INCR, 1, bin, aerospike_py.message.encode_payload(incr_by)[0])]
-        envelope = aerospike_py.message.pack_asmsg(0, flags, 0, 0, 0, 0,
+        envelope = aerospike_py.message.pack_asmsg(0, flags, 0, 0, record_ttl, 0,
             [
                 aerospike_py.message.pack_asmsg_field(namespace.encode('UTF-8'), aerospike_py.message.AS_MSG_FIELD_TYPE_NAMESPACE),
                 aerospike_py.message.pack_asmsg_field(hash_key(set, key), aerospike_py.message.AS_MSG_FIELD_TYPE_DIGEST_RIPE),
@@ -107,12 +107,12 @@ class AerospikeClient:
         return buckets
 
 
-    def _append_op(self, namespace, set='', key='', bin='', append_blob='', op=aerospike_py.message.AS_MSG_OP_APPEND):
+    def _append_op(self, namespace, set='', key='', bin='', append_blob='', op=aerospike_py.message.AS_MSG_OP_APPEND, record_ttl=0):
         flags = aerospike_py.message.AS_INFO2_WRITE
 
         blob = aerospike_py.message.encode_payload(append_blob)
         bin_cmds = [aerospike_py.message.pack_asmsg_operation(op, blob[1], bin, blob[0])]
-        envelope = aerospike_py.message.pack_asmsg(0, flags, 0, 0, 0, 0,
+        envelope = aerospike_py.message.pack_asmsg(0, flags, 0, 0, record_ttl, 0,
             [
                 aerospike_py.message.pack_asmsg_field(namespace.encode('UTF-8'), aerospike_py.message.AS_MSG_FIELD_TYPE_NAMESPACE),
                 aerospike_py.message.pack_asmsg_field(hash_key(set, key), aerospike_py.message.AS_MSG_FIELD_TYPE_DIGEST_RIPE),
@@ -128,14 +128,14 @@ class AerospikeClient:
 
         return buckets
 
-    def append(self, namespace, set='', key='', bin='', append_blob=''):
-        return self._append_op(namespace, set, key, bin, append_blob, aerospike_py.message.AS_MSG_OP_APPEND)
+    def append(self, namespace, set='', key='', bin='', append_blob='', record_ttl=0):
+        return self._append_op(namespace, set, key, bin, append_blob, aerospike_py.message.AS_MSG_OP_APPEND, record_ttl)
 
-    def prepend(self, namespace, set='', key='', bin='', append_blob=''):
-        return self._append_op(namespace, set, key, bin, append_blob, aerospike_py.message.AS_MSG_OP_PREPEND)
+    def prepend(self, namespace, set='', key='', bin='', append_blob='', record_ttl=0):
+        return self._append_op(namespace, set, key, bin, append_blob, aerospike_py.message.AS_MSG_OP_PREPEND, record_ttl)
 
-    def touch(self, namespace, set, key, bin=''):
-        return self._append_op(namespace, set, key, bin, None, aerospike_py.message.AS_MSG_OP_TOUCH)
+    def touch(self, namespace, set, key, bin='', record_ttl=0):
+        return self._append_op(namespace, set, key, bin, None, aerospike_py.message.AS_MSG_OP_TOUCH, record_ttl)
 
 
 def connect(host: str, port: int) -> AerospikeClient:
